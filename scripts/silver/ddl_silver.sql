3a1728f176462a0f02d7e948233751fb6ce096b0
Quân <24127228@student.hcmus.edu.vn>
2025-11-29 19:20:46 +0700

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,11 +1,11 @@
 /*
 ===============================================================================
-DDL Script: Create Silver Tables
+DDL Script: Create Silver Tables (FIXED)
 ===============================================================================
 Script Purpose:
     This script creates tables in the 'silver' schema, dropping existing tables 
     if they already exist.
-	  Run this script to re-define the DDL structure of 'silver' Tables
+    Run this script to re-define the DDL structure of 'silver' Tables
 ===============================================================================
 */
 
@@ -20,7 +20,8 @@ CREATE TABLE silver.crm_cust_info (
     cst_lastname        NVARCHAR(50),
     cst_marital_status  NVARCHAR(50),
     cst_gndr            NVARCHAR(50),
-    cst_create_date     DATE
+    cst_create_date     DATE,
+    dwh_create_date     DATETIME2 DEFAULT GETDATE() 
 );
 GO
 
@@ -29,15 +30,15 @@ IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
 GO
 
 CREATE TABLE silver.crm_prd_info (
-    prd_id       INT,
-	cat_id		 NVARCHAR(50),
-    prd_key      NVARCHAR(50),
-    prd_nm       NVARCHAR(50),
-    prd_cost     INT,
-    prd_line     NVARCHAR(50),
-    prd_start_dt DATE,
-    prd_end_dt   DATE,
-	dwh_create_date DATETIME2 DEFAULT
+    prd_id          INT,
+    cat_id          NVARCHAR(50),
+    prd_key         NVARCHAR(50),
+    prd_nm          NVARCHAR(50),
+    prd_cost        INT,
+    prd_line        NVARCHAR(50),
+    prd_start_dt    DATE,
+    prd_end_dt      DATE,
+    dwh_create_date DATETIME2 DEFAULT GETDATE() 
 );
 GO
 
@@ -46,15 +47,16 @@ IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
 GO
 
 CREATE TABLE silver.crm_sales_details (
-    sls_ord_num  NVARCHAR(50),
-    sls_prd_key  NVARCHAR(50),
-    sls_cust_id  INT,
-    sls_order_dt INT,
-    sls_ship_dt  INT,
-    sls_due_dt   INT,
-    sls_sales    INT,
-    sls_quantity INT,
-    sls_price    INT
+    sls_ord_num     NVARCHAR(50),
+    sls_prd_key     NVARCHAR(50),
+    sls_cust_id     INT,
+    sls_order_dt    INT,
+    sls_ship_dt     INT,
+    sls_due_dt      INT,
+    sls_sales       INT,
+    sls_quantity    INT,
+    sls_price       INT,
+    dwh_create_date DATETIME2 DEFAULT GETDATE() 
 );
 GO
 
@@ -63,8 +65,9 @@ IF OBJECT_ID('silver.erp_loc_a101', 'U') IS NOT NULL
 GO
 
 CREATE TABLE silver.erp_loc_a101 (
-    cid    NVARCHAR(50),
-    cntry  NVARCHAR(50)
+    cid             NVARCHAR(50),
+    cntry           NVARCHAR(50),
+    dwh_create_date DATETIME2 DEFAULT GETDATE() 
 );
 GO
 
@@ -73,9 +76,10 @@ IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
 GO
 
 CREATE TABLE silver.erp_cust_az12 (
-    cid    NVARCHAR(50),
-    bdate  DATE,
-    gen    NVARCHAR(50)
+    cid             NVARCHAR(50),
+    bdate           DATE,
+    gen             NVARCHAR(50),
+    dwh_create_date DATETIME2 DEFAULT GETDATE() 
 );
 GO
 
@@ -84,9 +88,10 @@ IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
 GO
 
 CREATE TABLE silver.erp_px_cat_g1v2 (
-    id           NVARCHAR(50),
-    cat          NVARCHAR(50),
-    subcat       NVARCHAR(50),
-    maintenance  NVARCHAR(50)
+    id              NVARCHAR(50),
+    cat             NVARCHAR(50),
+    subcat          NVARCHAR(50),
+    maintenance     NVARCHAR(50),
+    dwh_create_date DATETIME2 DEFAULT GETDATE() 
 );
 GO
